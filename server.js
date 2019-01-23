074aff657eb8360757e8fb7b71f0e399dd0bc330
ranja <ranjamick@gmail.com>
2019-01-23 17:12:51 +0300

    ddghgddd

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/blog-jaona/dist/blog-jaona'));
+app.use(express.static('blog-jaona/dist/blog-jaona'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('/blog-jaona/dist/blog-jaona/index.html'));
+res.sendFile(path.join('blog-jaona/dist/blog-jaona/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
